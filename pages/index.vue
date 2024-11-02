ccd71c4b67aa6735095f6fda5721f28f10ecb0e9
Radu Arsene <radu.c.arsene@gmail.com>
2024-11-02 13:25:22 +0200

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
         <header class="h-14 bg-green-600"></header>
-        <div>asdasd3r23r modif</div>
+        <div>{{ lowerLimit }} {{ upperLimit }}</div>
     </div>
 </template>
 
 <script setup>
+const points = 100
+const level = 6
+const lowerLimit = Math.pow(2, level)
+const upperLimit = Math.pow(2, level + 1)
 
+function calculateProgressPercentage(points) {
+    
+}
 </script>
